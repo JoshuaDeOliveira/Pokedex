96c64f6622168eb3c45f328357d8e29508a97040
JoshuaOliveira <joshuadultra@gmail.com>
2025-05-28 19:26:15 -0300

    Att com ultima pagina HTML

---
+++ Data/Scripts/Home.js
@@ -0,0 +1,5 @@
+document.querySelector('.National-Pokedex').addEventListener('click', () => {window.location.href = 'Home.html'})
+
+document.querySelector('.Pokedex-Interativa').addEventListener('click', () => {window.location.href = 'Pokedex.html'})
+
+document.querySelector('.Gerações').addEventListener('click', () => {window.location.href = 'Gerações.html'})
